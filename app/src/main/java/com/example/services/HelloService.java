5a3001ea25d5a4025c61398d9d03e87e8492a692
isilay-subasi <isil.subasi19@gmail.com>
2021-11-03 15:46:05 +0300

    Service Class

---
+++ app/src/main/java/com/example/services/HelloService.java
@@ -0,0 +1,71 @@
+package com.example.services;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class HelloService extends Service {
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID, "My app no sound", NotificationManager.IMPORTANCE_LOW
+            );
+            notificationChannel.setDescription("");
+            notificationChannel.setSound(null,null);
+            notificationChannel.enableLights(false);
+            notificationChannel.enableVibration(false);
+            notificationManager.createNotificationChannel(notificationChannel);
+            Notification notification = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+                    .setContentTitle("")
+                    .setContentText("").build();
+            startForeground(1, notification);
+        }
+
+
+
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Toast.makeText(this, "Servis başlatıldı", Toast.LENGTH_LONG).show();
+        return START_NOT_STICKY;
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Servis durduruldu", Toast.LENGTH_LONG).show();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            stopForeground(true);
+        }
+    }
+
+}
--- app/src/main/java/com/example/services/MainActivity.java
+++ app/src/main/java/com/example/services/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.services;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button btnStart;
+    public Button btnStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initThis();
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startHelloService();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopHelloService();
+            }
+        });
+
+
+
     }
+
+    public void initThis(){
+        btnStart=findViewById(R.id.startBtn);
+        btnStop=findViewById(R.id.stopBtn);
+
+    }
+
+    private void startHelloService() {
+        Intent intent = new Intent(getApplicationContext(),HelloService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(intent);
+        }else startService(intent);
+    }
+
+    private void stopHelloService() {
+
+        Intent intent = new Intent(getApplicationContext(),HelloService.class);
+        stopService(intent);
+
+    }
+
+
+
 }
